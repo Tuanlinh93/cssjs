86d77a3306e3e258c69f58e3a9328d54116a9f26
Tuanlinh93 <84119787+Tuanlinh93@users.noreply.github.com>
2021-05-13 21:13:37 +0700

    Add files via upload

---
+++ redirectcopy.js
@@ -0,0 +1,18 @@
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='
+	https://www.facebook.com/H%C6%B0ng-Th%E1%BB%8Bnh-Pearl-D%C4%A9-An-B%C3%ACnh-D%C6%B0%C6%A1ng-100250522248824'>https://www.facebook.com/H%C6%B0ng-Th%E1%BB%8Bnh-Pearl-D%C4%A9-An-B%C3%ACnh-D%C6%B0%C6%A1ng-100250522248824"+document.location.pathname+"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
